e0f7f527d910806fc8e6eb878959ee76ccf4b7bc
tunsol0292 <tunsol0292@gmail.com>
2025-07-30 23:37:51 +0900

    commit

--- homework0730/src/App.jsx
+++ homework0730/src/App.jsx
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './page/Home';
 
 function App() {
   return (
     <>
-    
+      <h1>위시템 담기🛒</h1>
+      <Home/>
     </>
   );
 }
---
+++ homework0730/src/page/Home.jsx
@@ -0,0 +1,11 @@
+import WishForm from "./wishform/WishForm";
+
+const Home = () => {
+    return (
+        <>
+            <WishForm/>
+        </>
+    );
+};
+
+export default Home;
